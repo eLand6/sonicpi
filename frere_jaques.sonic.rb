992a6bea70df7cc98ba260688b82b4f72cd97ead
eLand6 <elan@jonathandimond.com>
2016-01-24 22:20:39 +0000

    Make drums more efficient and flexible

--- frere_jaques.sonic.rb
+++ frere_jaques.sonic.rb
@@ -1,5 +1,5 @@
 # frere jacues
-frere1 = [:g4, :b4, :d4, 0.5, :a4, 0.5, :b4, 0.5, :g4, 0.5]
+frere1 = [:g4, 0.5, :a4, 0.5, :b4, 0.5, :g4, 0.5]
 # dormez tu?
 frere2 = [:b4, 0.5, :c5, 0.5, :d5, 1.0]
 # do do do do dooo dooo
@@ -7,8 +7,28 @@ frere3 = [:d5, 0.25, :e5, 0.25, :d5, 0.25, :c5, 0.25, :b4, 0.5, :g4, 0.5]
 # ding dang dong
 frere4 = [:g4, 0.5, :d4, 0.5, :g4, 1.0]
 
+
+#bass drum
+bass1 = [2,-6,1,-2,1,-8].ring
+#cymbal
+cymbal1 = [1,-8,1,-2,1,-6].ring
+
 use_synth :blade
-use_bpm 120
+use_bpm 80
+
+define :play_drums do |timing, instrument|
+  index = 0
+  loop do
+    time_or_amp = timing[index]
+    if time_or_amp > 0 # time_or_amp is an amplitude
+      sample instrument, amp: time_or_amp
+    else # time_or_amp is a time in 1/8th notes
+      sleep (0.125) * (0- time_or_amp)
+    end
+    index = index + 1
+  end
+end
+
 
 define :play_phrase_sleeper do |phrase|
   for note_or_rest in phrase do
@@ -29,17 +49,12 @@ define :play_phrase_sleeper do |phrase|
   end
 
 
-  live_loop :drums do
-    sample :drum_bass_hard, amp: 2
-    sample :drum_cymbal_closed
-    sleep 0.75
-    sample :drum_bass_hard
-    sleep 0.25
-    sample :drum_bass_hard
-    sample :drum_cymbal_closed
-    sleep 0.25
-    sample :drum_cymbal_closed
-    sleep 0.75
+  live_loop :bass do
+    play_drums bass1, :drum_bass_hard
+  end
+
+  live_loop :cymbal do
+    play_drums cymbal1, :drum_cymbal_closed
   end
 
   play_phrase_sleeper song
